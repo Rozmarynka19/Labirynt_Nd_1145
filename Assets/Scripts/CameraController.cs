712ef96e80113c2374c88a5ddac95bd1b17a85e4
Rozmarynka19 <morozmarynowska@gmail.com>
2023-09-24 13:17:19 +0200

    Implement player and camera movment.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    float mouseSensitivity = 100f;
+    Transform playerBody;
+    float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        playerBody = transform.parent;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY; // xRotation = xRotation - mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
