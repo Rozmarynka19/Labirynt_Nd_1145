a8ec71c3925301d0cea0deb1c81373dc6bf27fa0
Rozmarynka19 <morozmarynowska@gmail.com>
2023-10-01 13:19:58 +0200

    Implement gravity, time counting, game pausing.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float speed = 12f;
     CharacterController characterController;
+    [SerializeField]
+    float gravity = -9.81f;
+
+    public Transform groundChecker;
+    public LayerMask groundMask;
 
     private void Start()
     {
@@ -24,6 +29,41 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
+        CheckGroundType();
         characterController.Move(move * speed * Time.deltaTime);
+        ApplyGravity();
+    }
+
+    void ApplyGravity()
+    {
+        characterController.Move(Vector3.up * gravity * Time.deltaTime);
+    }
+
+    void CheckGroundType()
+    {
+        RaycastHit hitResult;
+        bool hit = Physics.Raycast(
+            groundChecker.position,
+            transform.TransformDirection(Vector3.down),
+            out hitResult,
+            0.4f,
+            groundMask);
+
+        if (hit)
+        {
+            string terrainType = hitResult.collider.gameObject.tag;
+            switch(terrainType)
+            {
+                case "Slow":
+                    speed = 3f;
+                    break;
+                case "Fast":
+                    speed = 20f;
+                    break;
+                default:
+                    speed = 12f;
+                    break;
+            }
+        }
     }
 }
